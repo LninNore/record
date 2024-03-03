456cf2afcfff165bd7c3a2c2eb6dfef6ab0dab4f
GGBond <908556557@qq.com>
2024-03-03 19:27:58 +0800

    你好世界

---
+++ src/test/java/per/me/record/RecordApplicationTests.java
@@ -0,0 +1,13 @@
+package per.me.record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
